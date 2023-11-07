cf08a20c9b542a5d6efdec2b75730471203f71b7
corsodotnet <ferreirarocha.br@gmail.com>
2023-11-07 12:55:06 +0100

    Addes constructor

--- Lessons/Classes/Program.cs
+++ Lessons/Classes/Program.cs
@@ -6,17 +6,35 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            Bank Ing = new();
-            Ing.Balance = 1000; // Error: Setter is private
-            Console.WriteLine($" Your balance is: {Ing.Balance}");
-            Console.WriteLine($" Your balance is: {Ing.Balance}");
+            Bank Intesa = new("Intesa", "str bla bla", "UK305949545", "Bruno"); ;
+            Bank Ing = new("Ing", "str bla bla", "UK305949545", "Bruno"); ;
+            Bank unicredit = new("unicredit", "str bla bla", "UK305949545", "Bruno"); ;
+            Bank Revolut = new("Revolut", "str bla bla", "UK305949545", "Bruno"); ;
+            //Intesa.Balance = 1000; // Error: Setter is private
+            Console.WriteLine($" Your balance is: {Intesa.Balance}");
+            Console.WriteLine($" The Bank's name is: {Intesa._name}");
+            Console.WriteLine($" The Bank's _address is: {Intesa._address}");
+            Console.WriteLine($" The Bank's _vatNumber is: {Intesa._vatNumber}");
+            Console.WriteLine($" The Bank's _CEO is: {Intesa._CEO}");
         }
     }
     public class Bank
     {
+        public string _name;
+        public string _address;
+        public string _vatNumber;
+        public string _CEO;
         private decimal _balance = 1000m;
         private decimal _interestRate = 5;
         private decimal _interests = 50m;
+
+        public Bank(string Name, string Address, string VatNumber, string CEO)
+        {
+            _name = Name;
+            _address = Address;
+            _vatNumber = VatNumber;
+            _CEO = CEO;
+        }
         public decimal Balance
         {
             get
@@ -29,7 +47,7 @@ namespace Classes
                 _balance = value;
             }
         }
-        decimal calcInterests()
+        private decimal calcInterests()
         {
             return _balance / 100 * _interestRate;
         }
