33192f91601ba12fc73dd33af6cfdea3afb3e205
corsodotnet <ferreirarocha.br@gmail.com>
2023-11-20 00:58:03 +0100

    #file project added

---
+++ Lessons/Exception.Lesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exception.Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
