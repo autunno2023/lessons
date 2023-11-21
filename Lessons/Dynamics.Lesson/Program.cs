207cfbf349a40ea0dcabf8bd81f8aac113d08835
corsodotnet <ferreirarocha.br@gmail.com>
2023-11-21 11:49:00 +0100

    #problemIncapsulation

---
+++ Lessons/Dynamics.Lesson/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Dynamics.Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            State italy = new State();
+            var response = italy.CreateRegion("Name");
+        }
+    }
+
+    internal class State
+    {
+        internal Region CreateRegion(string Name)
+        {
+            return new Region(Name);
+        }
+        private class Region
+        {
+            string _name;
+            public string Name { get => _name; }
+
+            public Region(string Name)
+            {
+                _name = Name;
+            }
+
+        }
+    }
+}
