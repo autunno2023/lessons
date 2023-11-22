29a9d73b90cc5d73056727c1c3ba6094dd99d3d5
corsodotnet <ferreirarocha.br@gmail.com>
2023-11-22 10:46:56 +0100

    Deleted some code

--- Lessons/Generics.Lesson/Program.cs
+++ Lessons/Generics.Lesson/Program.cs
@@ -9,24 +9,7 @@ namespace Generics.Lesson
     {
         static void Main(string[] args)
         {
-            List<Person> list = new List<Person>();
-            list.Add(new Person());
-            list.Add(new Person());
-            list.Add(new Person());
-            list.Add(new Person());
-            list.Add(new Person());
-            list.Add(new Person());
 
-
-            #region Generic List
-            MyGeneriList<Person> people = new MyGeneriList<Person>();
-            people.AddItem(new() { Name = "Bruno", Age = 40 });
-            people.AddItem(new() { Name = "Marco", Age = 30 });
-            people.AddItem(new() { Name = "Diego", Age = 20 });
-            people.AddItem(new() { Name = "Anna", Age = 10 });
-            people.AddItem(new() { Name = "Maria", Age = 24 });
-            people.AddItem(new() { Name = "Laura", Age = 50 });
-            #endregion
 
             #region Generic FileWriter  
 
