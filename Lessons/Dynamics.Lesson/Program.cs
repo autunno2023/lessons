b919379783ba1877725d0e95d9e95dd11a7c4597
corsodotnet <ferreirarocha.br@gmail.com>
2023-11-28 23:56:25 +0100

    #

--- Lessons/Dynamics.Lesson/Program.cs
+++ Lessons/Dynamics.Lesson/Program.cs
@@ -8,46 +8,14 @@ namespace Dynamics.Lesson
 
         static void Main(string[] args)
         {
-            #region Distrubiuto 
+            #region Deserialize 
             //getDto();
             getDynamicObj();
-            #endregion
+            #endregion 
+
 
 
             State italy = new State("Italy");
-            #region PublicInfo   
-            // Ritorno i membri PUBLIC dello State tramite Anonymous + dynamic 
-            Console.WriteLine("\n---- Ritorno i membri publici dello State tramite Anonymous + dynamic ----\n");
-
-            dynamic PublicInfo = italy.GetPublicInfoAnonymous();
-
-            Console.WriteLine(PublicInfo.Name);
-            Console.WriteLine(PublicInfo.Population);
-            Console.WriteLine(PublicInfo.Regioni[0].Name);
-            Console.WriteLine(PublicInfo.Regioni[1].Name);
-            Console.WriteLine(PublicInfo.Regioni[1].Province[0].Name);
-            #endregion
-            #region PrivateInfo  
-            // Ritorno i membri PRIVATE dello State tramite Anonymous + dynamic 
-            Console.WriteLine("\n----Ritorno i membri privati dello State tramite Anonymous + dynamic----\n");
-
-            dynamic PrivateInfo = italy.GetPrivateInfoByAnonymous();
-
-            Console.WriteLine(PrivateInfo._name);
-            Console.WriteLine(PrivateInfo._population);
-            Console.WriteLine(PrivateInfo._regioni[0].Name);
-            Console.WriteLine(PrivateInfo._regioni[1].Name);
-            Console.WriteLine(PrivateInfo._regioni[1].Province[0].Name);
-            #region Considerazioni 
-            /*  Anoynmous type non aggira le regole di visibilita dell'oggetto,
-                ma crea al volo un oggetto anonimo ci chiave/valore di tipo stringa. 
-                Il suo uso però a dei LIMITI. Se non si rispetta perfettamente la struttura degli oggetti (Ese: Annidamento)
-                si possono generare bug che saranno poi rilevabili solo a Runtime.
-
-            */
-            #endregion
-
-            #endregion
             #region StateDynamic 
             // Ritorno tutto l'oggetto State tramite Dynamic
 
@@ -74,6 +42,27 @@ namespace Dynamics.Lesson
 
 
             #endregion
+            #region PrivateInfo  
+            // Ritorno i membri PRIVATE dello State tramite Anonymous + dynamic 
+            Console.WriteLine("\n----Ritorno i membri privati dello State tramite Anonymous + dynamic----\n");
+
+            dynamic PrivateInfo = italy.GetPrivateInfoByAnonymous();
+
+            Console.WriteLine(PrivateInfo._name);
+            Console.WriteLine(PrivateInfo._population);
+            Console.WriteLine(PrivateInfo._regioni[0].Name);
+            Console.WriteLine(PrivateInfo._regioni[1].Name);
+            Console.WriteLine(PrivateInfo._regioni[1].Province[0].Name);
+            #region Considerazioni 
+            /*  Anoynmous type non aggira le regole di visibilita dell'oggetto,
+                ma crea al volo un oggetto anonimo ci chiave/valore di tipo stringa. 
+                Il suo uso però a dei LIMITI. Se non si rispetta perfettamente la struttura degli oggetti (Ese: Annidamento)
+                si possono generare bug che saranno poi rilevabili solo a Runtime.
+
+            */
+            #endregion
+
+            #endregion    
 
 
             Console.Read();
@@ -82,19 +71,17 @@ namespace Dynamics.Lesson
         static Person? getDto()
         {
             string jsonString = "{\"Name\":\"John\", \"Age\":30}";
-            Person? person = JsonConvert.DeserializeObject<Person>(jsonString);
+            Person? person = JsonConvert.DeserializeObject<Person>(jsonString); // tipizzato
             Console.WriteLine($"Name: {person?.Name},\n Age: {person?.Age}");
             return person;
         }
         static Person? getDynamicObj()
         {
             string jsonString = "{\"Fullname\":\"John Doe\", \"Age\":30}";
-            dynamic person = JsonConvert.DeserializeObject<Person>(jsonString); //  non Piu tipizzato ma Dinamico
+            dynamic? person = JsonConvert.DeserializeObject(jsonString); //  utilizza tipizzato ma Dinamico
             Console.WriteLine($"Name: {person?.Name ?? person?.Fullname}, Age: {person?.Age}");  // Coalescing Operators
             return person;
         }
-
-
     }
     class Person
     {
@@ -103,6 +90,7 @@ namespace Dynamics.Lesson
         public int? Age { get; set; }
 
     }
+
 
 }
 
