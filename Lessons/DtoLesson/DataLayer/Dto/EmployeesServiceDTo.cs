9cfb80074b2abbe05a5b90760d6b9e61df2cb93a
corsodotnet <ferreirarocha.br@gmail.com>
2023-12-02 18:30:53 +0100

    #

--- Lessons/DtoLesson/DataLayer/Dto/EmployeesServiceDTo.cs
+++ Lessons/DtoLesson/DataLayer/Dto/EmployeesServiceDTo.cs
@@ -8,17 +8,17 @@ namespace DataLayer.Dto
     {
         public EmployeesViewModelDTo ViewModel { get; set; }
 
-        public decimal Salary { get; set; }
-        public string Company { get; set; }
-        public bool IsLocked { get; set; }
-        public int JobContractId { get; set; }
+        public decimal? Salary { get; set; }
+        public string? Company { get; set; }
+        public bool? IsLocked { get; set; }
+        public int? JobContractId { get; set; }
         public string? Name { get; set; }
         public int? Id { get; set; }
-        internal EmployeesServiceDTo(Employee Employee)
+        internal EmployeesServiceDTo(Employee? Employee)
         {
-            Id = Employee.Id;
+            Id = Employee?.Id;
             IsLocked = false;
-            Name = Employee.Name;
+            Name = Employee?.Name;
             JobContractId = Employee?.ContactId ?? default(int);
             Salary = Employee?.JobContract?.Salary ?? default(decimal);
             Company = Employee?.JobContract?.Jobs?.CompanyName ?? string.Empty;
