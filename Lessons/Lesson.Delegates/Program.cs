6e87e2c7d943867406d9e20014b2c1831cf53dfa
corsodotnet <ferreirarocha.br@gmail.com>
2023-12-18 10:12:24 +0100

    #

--- Lessons/Lesson.Delegates/Program.cs
+++ Lessons/Lesson.Delegates/Program.cs
@@ -1,18 +1,87 @@
 ﻿using System;
+using System.Threading;
 
 namespace Lesson.Delegates
 {
-    public delegate string HealthDataDelegate();
+
+    public delegate void HealthDataDelegate();// 
+    public delegate string DataDelegate();// 
 
     internal class Program
     {
         static void Main(string[] args)
         {
             EUDigitalWallet eUDigitalWallet = new EUDigitalWallet("RTERYEHTHTRHTHRTY");
-            HealthDataDelegate healthData = eUDigitalWallet.getClinicalStatus;
 
+
+            HealthDataDelegate healthData = eUDigitalWallet.GetTaxInfo;
             AssicurazioneSanitaria.CreateNewAssurance(healthData);
+
+
+            // MultiDelegate 
+            DataDelegate datiSanitari, datiLegali, datiFiscali, multiDelegate;
+            // datiSanitari = eUDigitalWallet.getClinicalStatus;
+            datiLegali = eUDigitalWallet.GetPenalStatus;
+            datiFiscali = eUDigitalWallet.GetTaxInfo;
+
+            //multiDelegate = datiSanitari;
+            //multiDelegate += datiLegali;
+            //multiDelegate += datiFiscali;
+
+            // multiDelegate();// all 
+
+            //foreach (var del in multiDelegate.GetInvocationList())
+            //{
+            //    del.DynamicInvoke(); // One by one
+            //}
+
+            //multiDelegate += () =>
+            //{
+            //    string s = "Ciao Mondo";
+            //    Console.WriteLine(s);
+            //    return s;
+            //};
+
+
+
+            // Action -> get parameter - void (T t)
+            // Func ->   T (T1, T1, T2, T3)
+            // Prediate -> bool (T ) 
+
+
+            Action action = () => { Console.WriteLine("Ciao"); };
+            Action<string, int> actionS = (s, n) => { Console.WriteLine(s); };
+            Action<string> actionS2 = delegate (string s) { Console.WriteLine(s); };
+
+            // Func 
+
+            Func<int, int, bool, string> Randomfunc = (x, y, proceed) =>
+            {
+                Random rdn = new Random();
+                return proceed ? rdn.Next(x, y).ToString() : string.Empty;
+            };
+
+
+            string result = Randomfunc(10, 100, true);
+
+            // Predicate 
+            int[] ages = new int[] { 10, 20, 50 };
+            Predicate<int> filter = delegate (int age)
+            {
+                return age > 40 ? true : false;
+            };
+
+            int index = Array.IndexOf(ages, Array.Find(ages, filter));
+
+            TimerCallback timerCallback = new TimerCallback(Printtime);
+            Timer timer = new Timer(timerCallback, "Hello", 5000, 3000);
+
         }
+        public static void Printtime(object state)
+        {
+            Console.WriteLine("Time is {0}, Parameter is: {1}", DateTime.Now.ToLongTimeString(), state.ToString());
+        }
+
     }
 
     public class EUDigitalWallet
@@ -20,22 +89,22 @@ namespace Lesson.Delegates
         string _owner;
         string _diploma = "Maturità";
         string _taxInfo;
-        bool _penalStatus;
+        string _penalStatus;
         string _clinicalstatus = "Situazione sanitaria è OK";
         public EUDigitalWallet(string CF)
         {
             _owner = CF;
         }
 
-        public bool GetPenalStatus()
+        public string GetPenalStatus()
         {
             Console.WriteLine(_penalStatus); ;
             return _penalStatus;
         }
-        public string getClinicalStatus()
+        public void getClinicalStatus()
         {
             Console.WriteLine(_clinicalstatus); ;
-            return _clinicalstatus;
+
         }
         public string GetTaxInfo()
         {
@@ -54,9 +123,9 @@ namespace Lesson.Delegates
         {
 
         }
-        public static void CreateNewAssurance(HealthDataDelegate eUDigitalWallet)
+        public static void CreateNewAssurance(Func<string> eUDigitalWallet)
         {
-            eUDigitalWallet();
+            eUDigitalWallet(); //callback 
         }
     }
 }
