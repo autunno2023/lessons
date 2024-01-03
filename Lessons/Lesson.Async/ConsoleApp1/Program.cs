6581add5cc25337914104562913009f018ed7c51
corsodotnet <ferreirarocha.br@gmail.com>
2024-01-03 10:08:42 +0100

    #

---
+++ Lessons/Lesson.Async/ConsoleApp1/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Hospital hospital = new Hospital();
+
+            Patient Bruno = new Patient() { Name = "Bruno" };
+            Console.WriteLine($"Sending Patient {Bruno.Name.ToUpper()}...");
+            await hospital.DoctorAsyncParallel(Bruno);
+
+
+            Patient Luca = new Patient() { Name = "Luca" };
+            Console.WriteLine($"Sending Patient {Luca.Name.ToUpper()}...");
+            await hospital.DoctorAsyncParallel(Luca);
+
+
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+    public class Patient
+    {
+        public string Name { get; set; }
+    }
+
+    public class Hospital
+    {
+        public async Task<string> AnalysisLab()
+        {
+            Console.WriteLine($"AnalysisLab: started");
+            await Task.Delay(8000);
+            //Console.Out.WriteLine($"-------------------------------");
+            return "Blood OK";
+
+        }
+        public async Task<string> Radiology()
+        {
+
+            Console.WriteLine($"Radiology: started");
+            await Task.Delay(12000);
+            // Console.Out.WriteLine($"-------------------------------");
+
+
+            return "TAC OK";
+        }
+
+
+        public async Task DoctorAsyncParallel(Patient patient)
+        {
+
+            List<Task<string>> Results = new List<Task<string>>();
+
+
+
+            Task<string> RadiologyPromise = Radiology();
+            Task<string> AnalysisPromise = AnalysisLab();
+
+            Results.Add(RadiologyPromise);
+            Results.Add(AnalysisPromise);
+
+
+
+
+            ParallelCheckups(Results, patient);/// Runs Taks in  parallel [no Async]
+           // AsyncCheckups(Results, patient);/// Runs Taks in  parallel [ Async]
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            await Task.Run(async () =>
+            {
+
+                // Do a long task here while others tasks are running in parallel!  
+                Console.WriteLine($"DOCTOR CHECKUP -  STARTED for {patient.Name.ToUpper()}");
+                await Task.Delay(new Random().Next(5000, 10000));
+            });
+
+            Console.WriteLine($"DOCTOR CHECKUP - FINISHED for {patient.Name.ToUpper()}");
+
+
+            Console.ResetColor();
+            // WaitAllCheckupsAndDismiss(patient);
+            //  Console.WriteLine($"-------------------------------");
+
+
+        }
+
+        public async Task ParallelCheckups(List<Task<string>> tasks, Patient patient)
+        {
+            Console.WriteLine($"Parallel Checkups:: STARTED");
+
+            Console.Out.WriteLine($"...Awaiting for All the exames to complete for {patient.Name.ToUpper()}...");
+
+            var result = await Task.WhenAll(tasks); // Json  
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            Console.Out.WriteLine($"{result[0]} for {patient.Name.ToUpper()} ");
+            Console.Out.WriteLine($"-------------------------------");
+
+            Console.Out.WriteLine($"{result[1]} for {patient.Name.ToUpper()}");
+            Console.Out.WriteLine($"-------------------------------");
+            Console.ResetColor();
+            Console.WriteLine($"Parallel Checkups:: FINISHED for {patient.Name.ToUpper()}");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{patient.Name.ToUpper()} CAN GO HOME!");
+            Console.ResetColor();
+
+        }
+        public async void AsyncCheckups(List<Task<string>> tasks, Patient patient)
+        {
+
+            while (tasks.Count > 0)
+            {
+
+                Console.Out.WriteLine($"Awaiting for any of the remaining exames to complete  for {patient.Name.ToUpper()}...");
+
+                Task<string> finishedTask = await Task.WhenAny(tasks);
+                string result = await finishedTask;
+
+                if (result == "TAC OK")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{result} for {patient.Name.ToUpper()}");
+
+                    Console.ResetColor();
+                }
+                else if (result == "Blood OK")
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"{result} for {patient.Name.ToUpper()}");
+                    Console.ResetColor();
+                }
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                //Console.WriteLine($"-------------------------------");
+                Console.ResetColor();
+
+                // Remove the completed task from the list
+                tasks.Remove(finishedTask);
+            }
+
+            Console.WriteLine($"AsyncCheckups Finished for {patient.Name.ToUpper()}");
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{patient.Name.ToUpper()} CAN GO HOME!");
+            Console.ResetColor();
+
+            Console.WriteLine($"-------------------------------");
+
+
+        }
+    }
+}
